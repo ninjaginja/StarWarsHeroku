790c0a2968addb2d544a1387106b4ccd2ef9cf9d
Molly Wojcik <mollywojcik@gmail.com>
2018-02-24 14:02:53 -0700

    updated port

--- server.js
+++ server.js
@@ -91,5 +91,5 @@ app.post("/api/new", function(req, res) {
 // Starts the server to begin listening
 // =============================================================
 app.listen(port, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + port);
 });
